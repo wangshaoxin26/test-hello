50a1e2461fd8fa9094a3dd089f84323b0fa4da20
wangshaoxin26 <2403960938@qq.com>
2022-08-07 15:40:17 +0800

    初始化

--- src/main/java/com/aws/test/Application.java
+++ src/main/java/com/aws/test/Application.java
@@ -1,7 +1,11 @@
+package com.aws.test;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
