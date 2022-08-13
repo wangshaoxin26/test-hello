9276cc27274787c090d4f5531c1d35bdd2780d1a
wangshaoxin26 <2403960938@qq.com>
2022-08-13 18:26:53 +0800

    测试

--- src/main/java/com/aws/test/controller/HelloController.java
+++ src/main/java/com/aws/test/controller/HelloController.java
@@ -2,11 +2,13 @@ package com.aws.test.controller;
 
 import com.aws.test.service.TestTService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HelloController {
 
     @Autowired
